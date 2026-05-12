c998e29745a18697aa9ba83b13876b7ec67653b6
Ethan Hernandez <mrcereal295@gmail.com>
2026-05-12 18:21:47 -0400

    Update root_visualization.py

--- SciCompLibrary/RootFinding/root_visualization.py
+++ SciCompLibrary/RootFinding/root_visualization.py
@@ -12,18 +12,24 @@ def f(x):
 fig = plt.figure(figsize = (10, 5))
 plt.ion()
 plt.grid(True)
+
 plt.plot(x, x+np.sin(x))
 plt.show()
 plt.pause(2)
 root = 0
+zoom = 0
 for result in bi.bisection_iteration(f, -0.9, 1.0):
     if type(result) == tuple: # The recursion isn't done
-        plt.axvline(result[0])
-        plt.axvline(result[1])
+        plt.axvline(result[0], color='g')
+        plt.axvline(result[1], color='g')
+        if zoom % 5 == 0 and zoom > 1:
+            max_buffer = max(abs(result[0]/2), abs(result[1]/2))
+            plt.xlim(result[0]-max_buffer, result[1]+max_buffer)
         fig.canvas.draw()
         plt.pause(0.5)
     elif type(result) == float:
         root = result
+    zoom+=1
         
 
 print('Point found!')
